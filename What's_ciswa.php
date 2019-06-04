44e14bda039a693cc2f775ef61f7d305b7e5375a
dannyharvs <dannyharvs@users.noreply.github.com>
2019-06-04 14:36:32 +0800

    Update What's_ciswa.php

--- What's_ciswa.php
+++ What's_ciswa.php
@@ -16,14 +16,13 @@
 <div class="post single-page">
 		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
 
-		<div class="postcontent">
-			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-			<?php the_content(); ?>
-		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
-        </div><!--postcontet-->
 		
 	</div><!--post class-->
 	
+        <!—add the hook to the new tester text field which can be added to any div anywhere-->
+    
+	<p><?php the_field(‘tester’); ?></p>
+
    
 <?php endwhile; ?>
 			
