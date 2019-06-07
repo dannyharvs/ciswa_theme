786a33535230f77fedad7adcb1bbad9d15c7b863
dannyharvs <dannyharvs@users.noreply.github.com>
2019-06-07 12:17:32 +0800

    Update small_header.php

--- small_header.php
+++ small_header.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-      <meta charset="<?php bloginfo('charset'); ?>">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>
-         <?php
+        <?php
 		      if (function_exists('is_tag') && is_tag()) {
 		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
 		      elseif (is_archive()) {
@@ -25,20 +26,20 @@
 		      if ($paged>1) {
 		         echo ' - page '. $paged; }
 		   ?>
-</title>
-      
-<meta name="description" content="<?php bloginfo('description'); ?>">
+    </title>
 
-<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
-      
-    <?php wp_head(); ?> 
-    
-  </head>
-    
-      <body>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
 
-<div class="container-fluid banner">
-      <nav class="navbar">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <!-- <nav class="navbar">
         
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
@@ -91,13 +92,41 @@
       
        </ul> 
       </div>
-    </nav>      
-   
-      <header class="container-fluid">
-      <div class="container">
-          <h1> Professional development for people who support international students, for over 50 years</h1>
-          <button type="button" href="#" class="btn btn-default">Discover More</button>
-          </div>
-          </header>
- 
-          </div>
+    </nav>  -->
+
+        <nav class="navbar ">
+
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="http://"><img src="http://localhost/wordpress/wp-content/themes/ciswa_theme/images/CISWALogowhite.png" alt="CISWA logo" /></a>
+
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+
+                    <?php
+wp_nav_menu( array(
+'menu' => 'Primary',
+'depth' => 2,
+'container' => false,
+'menu_class' => 'nav navbar-nav navbar-right',
+'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+//Process nav menu using our custom nav walker
+'walker'=> new WP_Bootstrap_Navwalker())
+);
+?>
+                </div>
+            </div>
+        </nav>
+
+        <header class="container-fluid">
+            <div class="container">
+
+            </div>
+        </header>
+
+    </div>
