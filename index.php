d82e190909a4ddf5fced7bae84684aa58c727070
dannyharvs <dannyharvs@users.noreply.github.com>
2019-06-07 12:54:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,58 +6,41 @@ endif; ?>
 <div class="container-fluid maincontainer">
     <div class="container-fluid text-center">
         <h2 class="text-center">Our Mission</h2>
-
-
-        <article class="excerpts">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-            <div <?php post_class(); ?>>
-                <h3 class="posttitle" id="post-<?php the_ID(); ?>">
-                    <a href="<?php the_permalink() ?>" rel="bookmark">
-                        <?php the_title(); ?>
-                    </a>
-                </h3>
-
-                <div class="postcontent">
-                    <?php the_excerpt(); ?>
-                </div>
-                <!--postcontet-->
-
-
+        
+        <div class="row">
+            
+            
+            
+  <div class="col-sm-4">
+      <img class="img-responsive" src="http://localhost/wordpress/wp-content/themes/ciswa_theme/images/PD.jpg" alt="proffesional development"/>
+      <h3>Professional Development</h3>
+      <p>Providing education and support at our annual winter conference.</p>
+      <button type="button" class="btn btn-default">Register Now</button>
             </div>
-            <!--post class-->
-
-            <?php endwhile; ?>
-
-            <?php else: ?>
-            <div class="post">
-                <p>Sorry, no posts found.</p>
+  <div class="col-sm-4">
+      <img class="img-responsive" src="http://localhost/wordpress/wp-content/themes/ciswa_theme/images/awards.jpg" alt="CISWA awards"/>
+      <h3>CISWA Awards</h3>
+      <p>For promoting and supporting international education in WA.</p>
+      <button type="button" class="btn btn-default">View Awards</button>
             </div>
-            <!--post-->
-            <?php endif; ?>
-
-        </article>
-
-        <section class="aside_elements">
-            <!-- ---- Widgetized sidebar ------------------------->
-            <?php /* Widgetized sidebar */
-    if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('widgetized-area') ) : ?><?php endif; ?>
-            <!-- ------- Widgetized sidebar ---------------------->
-        </section>
-
-
-
-    </div>
+  <div class="col-sm-4">
+      <img class="img-responsive" src="http://localhost/wordpress/wp-content/themes/ciswa_theme/images/communityengagement.jpg" alt="community engagement"/>
+      <h3>Community Engagement</h3>
+      <p>Creating opportunities for international students.</p>
+      <button type="button" class="btn btn-default">Learn More</button>
+            </div>
 </div>
-
-
-<div class="container-fluid members text-center">
-    <div class="members">
-        <h3>Members</h3>
-
-        <?php echo do_shortcode('[logoshowcase]'); ?>
-
-    </div>
+      </div>
 </div>
-
+      
+      
+      <div class="container-fluid members text-center"> 
+          <div class="members">
+      <h3>Members</h3>
+              
+<?php echo do_shortcode('[logoshowcase]'); ?>
+              
+      </div>  
+      </div>
+      
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
