a6c425ca6f2c4e242bf3d06425b23c280bf6390a
dannyharvs <dannyharvs@users.noreply.github.com>
2019-06-11 13:49:28 +0800

    Create community_interactions.php

---
+++ community_interactions.php
@@ -0,0 +1,21 @@
+<?php
+    /*
+    Template Name: Community Interactions
+
+    */
+    ?>
+<?php include (TEMPLATEPATH . '/small_header.php');  /* Tells WordPress to include the smaller header header.php */ ?>
+
+<div class="container-fluid maincontainer">
+    <div class="container-fluid text-center">
+        <div class="container-fluid text-center heading">
+            <h2><?php the_field('ourmission'); ?></h2>
+        </div>
+        <div class="row">
+
+            <div class="col-sm-4">
+                
+</div>
+
+
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
